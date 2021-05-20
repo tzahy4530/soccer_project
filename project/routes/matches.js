6b8dabbe597a5cae06ec621607408de484a93440
tzahy4530 <tzahy4530@gmail.com>
2021-05-20 17:31:54 +0300

    bug Fixing.

--- project/routes/matches.js
+++ project/routes/matches.js
@@ -1,3 +1,4 @@
+
 var express = require("express");
 var router = express.Router();
 const DButils = require("./utils/DButils");
@@ -22,7 +23,25 @@ router.get("/stage/:stageId", async (req, res, next) => {
     const match_details = await matches_utils.getAllMatchesInfoByStageId(
       req.params.stageId
     )
-    //we should keep implementing team page.....
+        
+    for (match_info in match_details){  
+      const match_id = await DButils.execQuery(
+       `INSERT INTO dbo.matches (date, hour, host_team, away_team, referee_id, stadium, home_goal, away_goal)
+        VALUES ('${match_info.date}', '${match_info.hour}','${match_info.home_team}',
+        '${match_info.away_team}', '${match_info.referee_id}','${match_info.stadium}', '${match_info.results.home_goal}',
+        '${match_info.results.away_goal}');
+        SELECT SCOPE_IDENTITY() as id;`
+      );
+      
+      for (e in match_info.events){
+        await DButils.execQuery(
+          `INSERT INTO dbo.events (match_id, description)
+           VALUES ('${match_id.id}', '${e.event}';`
+         );  
+      } 
+    }
+    
+
     res.send(match_details);
   } catch (error) {
     next(error);
