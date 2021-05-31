a106a29ab040641c3e13674c6526eeb38134d34a
tzahy4530 <tzahy4530@gmail.com>
2021-05-31 10:48:22 +0300

    init example of test.

--- tests/association_users/checkValidDateForRefereeAppointment.test.js
+++ tests/association_users/checkValidDateForRefereeAppointment.test.js
@@ -1,13 +1,11 @@
 const ass_users_utils = require('../../project/routes/utils/association_users_utils')
 
-test('Check valid date for referee WHERE refere is busy.', () =>
+
+test('Check valid date for referee WHERE refere is busy in the same game.', async () =>
 {
-    
     referee_user_id = 8
     match_id = 1
-    ass_users_utils.checkValidDateForRefereeAppointment(8,1).then(
-        value => {expect(1+1).toBe(2)} 
-    )
-    
+    const valid = await ass_users_utils.checkValidDateForRefereeAppointment(8,1)
+    return expect(valid).toBe(false)
 })
 
