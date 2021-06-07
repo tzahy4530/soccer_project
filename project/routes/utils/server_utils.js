c81241276347c237c8d47eeec0567fa3fb30c759
isgales <alkalayg@post.bgu.ac.il>
2021-06-07 14:04:51 +0300

    secret

--- project/routes/utils/server_utils.js
+++ project/routes/utils/server_utils.js
@@ -8,13 +8,13 @@ var app = express();
 require("dotenv").config();
 const DButils = require("./DButils");
 console.log(process.env.COOKIE_SECRETS);
-console.log(process.env.fanRole);
+console.log(process.env.secret);
 app.use(logger("dev")); //logger
 app.use(express.json()); // parse application/json
 app.use(
   session({
     cookieName: "session", // the cookie key name
-    secret: process.env.COOKIE_SECRETS || `$COOKIE_SECRET`, // the encryption key
+    secret: process.env.COOKIE_SECRETS || process.env.secret, // the encryption key
     duration: 24 * 60 * 60 * 1000, // expired after 20 sec
     activeDuration: 1000 * 60 * 5, // if expiresIn < activeDuration,
     cookie: {
